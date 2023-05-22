dbfad0f55f13220a7957aadf5231094054d62f67
Stanislav Orlov <stanislav.orlov@ukma.edu.ua>
2023-05-22 15:25:41 +0300

    Search and training setup fragments

---
+++ app/src/main/java/com/example/practiceeng/fragments/WordSearchFragment.kt
@@ -0,0 +1,57 @@
+package com.example.practiceeng.fragments
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
+import androidx.fragment.app.Fragment
+import com.example.practiceeng.R
+import com.example.practiceeng.databinding.FragmentWordSearchBinding
+
+
+class WordSearchFragment : Fragment() {
+    private var _binding: FragmentWordSearchBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentWordSearchBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            wordSearch.setOnQueryTextListener(object : android.widget.SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    binding.textView.text = "Text submit: " + query
+                    return true
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    //Log.d("SEARCH", "Query: " + newText)
+                    return true
+                }
+
+            })
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding=null
+    }
+}
+
+
