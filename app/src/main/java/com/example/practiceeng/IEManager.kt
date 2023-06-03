9404454b3990525c888452407493d2db3556025c
Artemii Kolomiichuk <95016660+ArtemiiKolomiichuk@users.noreply.github.com>
2023-06-03 21:28:14 +0300

    Fix IEManager

--- app/src/main/java/com/example/practiceeng/IEManager.kt
+++ app/src/main/java/com/example/practiceeng/IEManager.kt
@@ -15,15 +15,17 @@ class IEManager {
 
         fun exportCards(cards: Array<WordCard>): String{
             val separator = "|||"
+            val separator2 = "@@@"
+            val separator3 = "\n#\n"
             val strBuilder = StringBuilder()
             strBuilder.append("[MeanintorWordCards]\n")
             for(card in cards){
                 strBuilder.append(card.word.word).append(separator)
                 strBuilder.append(card.partOfSpeech).append(separator)
                 strBuilder.append(card.definition).append(separator)
-                strBuilder.append(card.examples.joinToString(separator)).append(separator)
-                strBuilder.append(card.synonyms.joinToString(separator)).append(separator)
-                strBuilder.append(card.antonyms.joinToString(separator)).append(separator)
+                strBuilder.append(card.examples.joinToString(separator2)).append(separator)
+                strBuilder.append(card.synonyms.joinToString(separator2)).append(separator)
+                strBuilder.append(card.antonyms.joinToString(separator2)).append(separator3)
             }
             return strBuilder.toString()
         }
@@ -32,30 +34,51 @@ class IEManager {
          * @param separators Array of separators that separate the [parts] of the card,
          * the last separator is the one that separates cards
          */
-        fun importCards(data: String, separators: Array<String>, parts: Array<WordCardPart>, folderID: UUID){
+        fun extractCards(data: String, separators: Array<String>, parts: Array<WordCardPart>) : MutableList<WordCard>{
             val cards : MutableList<WordCard> = mutableListOf()
             val cardsData = data.split(separators[separators.size - 1])
             for(cardData in cardsData){
-                val card = WordCard("","", word = Word(""), folderID = folderID)
+                val card = WordCard("","", word = Word(""), folderID = null)
                 var chunk = cardData
                 var skipSeparators = 0
                 for (i in 0 until parts.size - 1) {
-                    val piece = chunk.substringBefore(separators[i + skipSeparators])
-                    chunk = chunk.substringAfter(separators[i + skipSeparators])
                     when (parts[i]) {
-                        WordCardPart.Word -> card.word = Word(piece)
-                        WordCardPart.PartOfSpeech -> card.partOfSpeech = piece
-                        WordCardPart.Definition -> card.definition = piece
+                        WordCardPart.Word -> {
+                            val piece = chunk.substringBefore(separators[i + skipSeparators])
+                            chunk = chunk.substringAfter(separators[i + skipSeparators])
+                            card.word = Word(piece)
+                        }
+                        WordCardPart.PartOfSpeech -> {
+                            val piece = chunk.substringBefore(separators[i + skipSeparators])
+                            chunk = chunk.substringAfter(separators[i + skipSeparators])
+                            card.partOfSpeech = piece
+                        }
+                        WordCardPart.Definition -> {
+                            val piece = chunk.substringBefore(separators[i + skipSeparators])
+                            chunk = chunk.substringAfter(separators[i + skipSeparators])
+                            card.definition = piece
+                        }
+
+
                         WordCardPart.Examples -> {
-                            card.examples = piece.split(separators[i + skipSeparators + 1]).toTypedArray()
+                            val piece = chunk.substringBefore(separators[i + skipSeparators + 1])
+                            chunk = chunk.substringAfter(separators[i + skipSeparators + 1])
+
+                            card.examples = piece.split(separators[i + skipSeparators]).toTypedArray()
                             skipSeparators++
                         }
                         WordCardPart.Synonyms -> {
-                            card.synonyms = piece.split(separators[i + skipSeparators + 1]).toTypedArray()
+                            val piece = chunk.substringBefore(separators[i + skipSeparators + 1])
+                            chunk = chunk.substringAfter(separators[i + skipSeparators + 1])
+
+                            card.synonyms = piece.split(separators[i + skipSeparators]).toTypedArray()
                             skipSeparators++
                         }
                         WordCardPart.Antonyms -> {
-                            card.antonyms = piece.split(separators[i + skipSeparators + 1]).toTypedArray()
+                            val piece = chunk.substringBefore(separators[i + skipSeparators + 1])
+                            chunk = chunk.substringAfter(separators[i + skipSeparators + 1])
+
+                            card.antonyms = piece.split(separators[i + skipSeparators]).toTypedArray()
                             skipSeparators++
                         }
                     }
@@ -68,14 +91,18 @@ class IEManager {
                     WordCardPart.Synonyms -> card.synonyms = chunk.split(separators[separators.size - 2]).toTypedArray()
                     WordCardPart.Antonyms -> card.antonyms = chunk.split(separators[separators.size - 2]).toTypedArray()
                 }
-                cards.add(card)
+                if(card.word.word.isNotBlank()){
+                    cards.add(card)
+                }
             }
-            //TODO: Move cards to specified folder
+            return cards
         }
 
         fun importCards(data: String, folderID: UUID){
             if(hasHeader(data)){
-                importCards(data, arrayOf("|||"), arrayOf(WordCardPart.Word, WordCardPart.PartOfSpeech, WordCardPart.Definition, WordCardPart.Examples, WordCardPart.Synonyms, WordCardPart.Antonyms), folderID)
+                val cards = extractCards(data.substringAfter("[MeanintorWordCards]\n"), arrayOf("|||","|||","|||","@@@","|||","@@@","|||","@@@","\n#\n"
+                ), arrayOf(WordCardPart.Word, WordCardPart.PartOfSpeech, WordCardPart.Definition, WordCardPart.Examples, WordCardPart.Synonyms, WordCardPart.Antonyms))
+                //TODO: Move to folder
             }
         }
 
