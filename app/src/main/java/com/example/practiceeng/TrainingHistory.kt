423ec8c421853183522701513c1d98b35f8b407f
Artemii Kolomiichuk <95016660+ArtemiiKolomiichuk@users.noreply.github.com>
2023-06-07 14:50:40 +0300

    Fix TrainingHistory

--- app/src/main/java/com/example/practiceeng/TrainingHistory.kt
+++ app/src/main/java/com/example/practiceeng/TrainingHistory.kt
@@ -11,6 +11,16 @@ constructor(
     /**
      * [TestType]s and the amount of times they have been completed
      */
-    var types : Array<Pair<TestType, Int>> = arrayOf(),
+    var types : Array<Pair<TestType, Int>> = arrayOf(
+        Pair(TestType.FlashCard, 0),
+        Pair(TestType.TrueFalse, 0),
+        Pair(TestType.MultipleChoiceWord, 0),
+        Pair(TestType.MultipleChoiceDefinition, 0),
+        Pair(TestType.Match, 0),
+        Pair(TestType.Synonyms, 0),
+        Pair(TestType.Antonyms, 0),
+        Pair(TestType.Writing, 0),
+        Pair(TestType.WritingListening, 0)
+    ),
     var lastDate : Date? = null)
 
